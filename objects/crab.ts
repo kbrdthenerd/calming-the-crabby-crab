90c06cc43c42f91fc40d0be4804c8a346ad83676
Katherine Brennan <katherinepbrennan@gmail.com>
2020-08-16 21:35:28 +0100

    fading crab

--- objects/crab.ts
+++ objects/crab.ts
@@ -8,21 +8,53 @@
 export class Crab extends Phaser.GameObjects.Image {
   private comfortKey: Phaser.Input.Keyboard.Key
   private color: Phaser.Display.Color
+  private happyLevel: number
 
   constructor(params) {
     super(params.scene, params.x, params.y, params.key, params.frame)
-    this.color = new Phaser.Display.Color(120, 0, 120)
+    this.happyLevel = 1
+    this.color = new Phaser.Display.Color(127.5, 0, 127.5)
     this.comfortKey = this.scene.input.keyboard.addKey(
       Phaser.Input.Keyboard.KeyCodes.SPACE
     )
     this.comfortKey.on('down', () => {
-      this.color = new Phaser.Display.Color(this.color.red + 10, this.color.green, this.color.blue)
+      this.changeColor(10)
     })
+
+    const TimerEventConfig = {
+      delay: 1000,
+      loop: true,
+      callback: this.randomColorChange,
+      callbackScope: this
+    }
+    
+    this.scene.time.addEvent(TimerEventConfig)
 
     this.scene.add.existing(this)
   }
 
+  randomColorChange() {
+    this.changeColor(-Math.floor(Math.random() * 10))
+  }
+
   update(): void {
+    const { color: { red, blue } } = this
+    if (Math.abs(red - blue) > 50) { 
+      this.happyLevel-= 0.01
+    } else if (Math.abs(red - blue) < 50) this.happyLevel+= 0.01
     this.tint = this.color.color
+    this.alpha = this.happyLevel
+  }
+
+  changeColor(amount): void {
+    const { color: { red, green, blue } } = this
+    this.color = new Phaser.Display.Color(red + amount, green, Math.max(0, blue - amount))
   }
 }
+
+
+/** 
+Math notes:
+
+total is always 255, so use a percentage
+*/
